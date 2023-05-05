b2916db7bd0c50cf2d2655416b0924307b629e00
B NGUYEN <bnguyen200982@gmail.com>
2023-05-05 16:56:54 +0000

    Add another line

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 class Hello {
   public static void main(String[] args) {
     System.out.println("This is a test java application");
+    System.out.println("Added another line to the console");
   }
 }
